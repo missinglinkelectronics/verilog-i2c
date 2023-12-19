de58696e96199a2af8efe4f9513ac5001f019c8e
Tobias Binkowski <tobias.binkowski@missinglinkelectronics.com>
2023-12-19 21:18:01 +0100

    i2c_mux: rework to handle for real-world io-ports

--- rtl/i2c_mux.v
+++ rtl/i2c_mux.v
@@ -32,6 +32,7 @@ THE SOFTWARE.
 
 module i2c_mux #(
     parameter FILTER_LEN = 4,
+    parameter RISE_LEN = 8,
     parameter DEV_ADDR = 7'h70,
     parameter PORTS = 4
 )
@@ -84,40 +85,65 @@ i2c_single_reg #(
     .data_out(mux_reg)
 );
 
+reg en;
 reg [PORTS-1:0] port;
+
+reg [PORTS-1:0] master_scl_out;
+reg [PORTS-1:0] master_sda_out;
+reg master_scl_in;
+reg master_sda_in;
 
 reg slave_scl_r;
 reg slave_sda_r;
-reg [PORTS-1:0] master_scl_r;
-reg [PORTS-1:0] master_sda_r;
+reg [RISE_LEN-1:0] master_scl_r;
+reg [RISE_LEN-1:0] master_sda_r;
 
-assign slave_scl_o = slave_scl_t;
-assign slave_sda_o = slave_sda_t;
-assign master_scl_o = master_scl_t;
-assign master_sda_o = master_sda_t;
+assign slave_scl_o = slave_scl_r;
+assign slave_sda_o = slave_sda_r;
+assign master_scl_o = master_scl_out;
+assign master_sda_o = master_sda_out;
 
 assign slave_scl_t = slave_scl_r;
 assign slave_sda_t = slave_sda_r;
-assign master_scl_t = master_scl_r;
-assign master_sda_t = master_sda_r;
+assign master_scl_t = master_scl_out;
+assign master_sda_t = master_sda_out;
 
 assign selected_port = port;
 
-always @(posedge clk) begin
-    master_scl_r <= {PORTS{1'h1}};
-    master_sda_r <= {PORTS{1'h1}};
-    slave_scl_r <= mux_scl_t || mux_scl_o;
-    slave_sda_r <= mux_sda_t || mux_sda_o;
+/* select master port if a port is selected */
+always @(*) begin
+    master_scl_out = {PORTS{1'b1}};
+    master_sda_out = {PORTS{1'b1}};
+    master_scl_in = 1'b1;
+    master_sda_in = 1'b1;
+    if (en) begin
+        master_scl_out[port] = master_scl_r[0];
+        master_sda_out[port] = master_sda_r[0];
+        master_scl_in = master_scl_i[port];
+        master_sda_in = master_sda_i[port];
+    end
+end
 
+always @(posedge clk) begin
+    port <= 0;
+    en <= 1'b0;
     if (mux_reg >= 4 && mux_reg < PORTS + 4) begin
         port <= mux_reg - 4;
-        master_scl_r[mux_reg-4] <= slave_scl_i || !slave_scl_t;
-        master_sda_r[mux_reg-4] <= slave_sda_i || !slave_sda_t;
-        slave_scl_r <= (master_scl_i[mux_reg-4] || !master_scl_t[mux_reg-4]) && (mux_scl_t || mux_scl_o);
-        slave_sda_r <= (master_sda_i[mux_reg-4] || !master_sda_t[mux_reg-4]) && (mux_sda_t || mux_sda_o);
-    end else begin
-        port <= 0;
+        en <= 1'b1;
     end
+end
+
+/* need to do some filtering when going from driving low to high-z with real IO */
+wire master_scl_filter;
+wire master_sda_filter;
+assign master_scl_filter = (master_scl_r != {RISE_LEN{1'b1}}) ? 1'b1 : 1'b0;
+assign master_sda_filter = (master_sda_r != {RISE_LEN{1'b1}}) ? 1'b1 : 1'b0;
+
+always @(posedge clk) begin
+    master_scl_r <= {master_scl_r[RISE_LEN-2:0], (slave_scl_i || !slave_scl_r)};
+    master_sda_r <= {master_sda_r[RISE_LEN-2:0], (slave_sda_i || !slave_sda_r)};
+    slave_scl_r <= (master_scl_in || master_scl_filter) & (mux_scl_t | mux_scl_o);
+    slave_sda_r <= (master_sda_in || master_sda_filter) & (mux_sda_t | mux_sda_o);
 end
 
 endmodule
